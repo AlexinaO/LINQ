49f75b901dbbdffd772c6d756556258aeb1748f4
AlexinaO <alexina.orsoni@gmail.com>
2018-08-06 16:50:13 +0200

    Rechercher le nombre d'occurence d'une lettre dans un mot

---
+++ Mammouth/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mammouth
+{
+    class Program
+    {
+        public static readonly string mot = "mammouth";
+
+        static void Main(string[] args)
+        {
+            FrequenceLettre();
+            Console.ReadKey();
+        }
+
+        //requete pour renvoyer la fréquence de chaque lettre
+        public static void FrequenceLettre()
+        {
+
+
+            var requete = from lettre in mot //boucle sur chaque lettre du mot
+                          group lettre by lettre into groupe
+                          //orderby groupe.Key ascending
+                          select new
+                          {
+                              Lettre = groupe.Key,
+                              NbLettres = groupe.Count()
+                          };
+            foreach (var resultat in requete)
+            {
+                Console.WriteLine($"{resultat.Lettre} + {resultat.NbLettres}");
+                
+                Console.WriteLine("*********");
+            }
+
+        }
+
+    }
+}
+
